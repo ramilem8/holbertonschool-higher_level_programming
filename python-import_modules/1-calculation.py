cd29e93e72133bcc7575337b75d6859edddbe2e8
ramilem8 <13728@holbertonstudents.com>
2026-04-14 21:44:49 +0000

    add

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -6,14 +6,8 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    result = calculator_1.add(a, b)
-    print(f"{a} + {b} = {result}")
-
-    result = calculator_1.sub(a, b)
-    print(f"{a} - {b} = {result}")
-
-    result = calculator_1.mul(a, b)
-    print(f"{a} * {b} = {result}")
-
-    result = calculator_1.div(a, b)
-    print(f"{a} / {b} = {result}")
+    # Call and print results for each operation
+    print(f"{a} + {b} = {calculator_1.add(a, b)}")
+    print(f"{a} - {b} = {calculator_1.sub(a, b)}")
+    print(f"{a} * {b} = {calculator_1.mul(a, b)}")
+    print(f"{a} / {b} = {calculator_1.div(a, b)}")
