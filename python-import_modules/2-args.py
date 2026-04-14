85c0bb2dcfb3a663e93da0e99489b1aa1e27ff02
ramilem8 <13728@holbertonstudents.com>
2026-04-14 21:54:46 +0000

    add

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,10 +2,8 @@
 import sys
 
 if __name__ == "__main__":
-    # Skriptin adı çıxılmaqla arqumentlərin sayı
     count = len(sys.argv) - 1
 
-    # İlk sətrin formatlanması
     if count == 0:
         print("0 arguments.")
     elif count == 1:
@@ -13,6 +11,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
 
-    # Hər bir arqumentin mövqeyi və dəyəri
     for i in range(1, count + 1):
         print("{}: {}".format(i, sys.argv[i]))
