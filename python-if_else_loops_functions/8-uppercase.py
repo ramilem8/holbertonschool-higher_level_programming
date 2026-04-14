54c151a582b3de7c5e32d1aecb6f00c463dd376f
ramilem8 <13728@holbertonstudents.com>
2026-04-14 20:40:31 +0000

    add

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
+    """Prints a string in uppercase followed by a new line."""
     for char in str:
-        # Karakter küçük harf ise (a-z), ASCII farkını kullanarak büyüt (a=97, A=65)
         if ord(char) >= 97 and ord(char) <= 122:
             char = chr(ord(char) - 32)
         print("{}".format(char), end="")
