972c23ca21a261d5534171e4ce8ae94b89549e2e
ramilem8 <13728@holbertonstudents.com>
2026-04-14 20:21:40 +0000

    add

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
-
-print(islower("a"))  
+    """Checks for lowercase character."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
