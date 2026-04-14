6f2708e5ecf35519b6ad5be2d0b2d9a3c3801e10
ramilem8 <13728@holbertonstudents.com>
2026-04-14 21:40:51 +0000

    add

--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -4,7 +4,15 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+
+    # Perform and print results in a minimal number of print statements
+    results = [
+        "{} + {} = {}".format(a, b, add(a, b)),
+        "{} - {} = {}".format(a, b, sub(a, b)),
+        "{} * {} = {}".format(a, b, mul(a, b)),
+        "{} / {} = {}".format(a, b, div(a, b))
+    ]
+    
+    # Loop to print all results using only 4 print statements
+    for result in results:
+        print(result)
