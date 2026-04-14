8ad7b679e8da0e52150cafd504fcde462afdf4ae
ramilem8 <13728@holbertonstudents.com>
2026-04-14 19:58:56 +0000

    add

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    """Karakterin küçük harf olup olmadığını kontrol eden fonksiyon."""
+    """Checks for lowercase character."""
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
