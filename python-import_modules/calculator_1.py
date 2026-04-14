634fe3254a5e87b2b0acfef4d5561258840afc98
ramilem8 <13728@holbertonstudents.com>
2026-04-14 21:39:29 +0000

    add

--- python-import_modules/calculator_1.py
+++ python-import_modules/calculator_1.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-def add(a, b):
+from calculator_1 import add, sub, mul, div
 
-    return (a + b)
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-
-def sub(a, b):
-   
-    return (a - b)
-
-
-def mul(a, b):
-    return (a * b)
-
-
-def div(a, b):
-    return int(a / b)
+    # Call and print the result of each function
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
