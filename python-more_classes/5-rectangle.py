d41fc6c245f5b0c4868b457fa8873d19fea8da85
ramilem8 <13728@holbertonstudents.com>
2026-04-29 10:59:15 +0000

    addd

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """Defines a Rectangle class."""
+
+
 class Rectangle:
     """Represents a rectangle."""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
     @property
     def width(self):
         """Get width."""
         return self.__width
+
     @width.setter
     def width(self, value):
         """Set width."""
@@ -17,10 +22,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         """Get height."""
         return self.__height
+
     @height.setter
     def height(self, value):
         """Set height."""
@@ -29,22 +36,27 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         """Return area."""
         return self.__width * self.__height
+
     def perimeter(self):
         """Return perimeter."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
     def __str__(self):
         """Print rectangle using #."""
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join("#" * self.__width for _ in range(self.__height))
+
     def __repr__(self):
-        """Return string representation for eval()."""
+        """Return string representation."""
         return f"Rectangle({self.__width}, {self.__height})"
+
     def __del__(self):
-        """Print message when instance is deleted."""
+        """Print message on deletion."""
         print("Bye rectangle...")
