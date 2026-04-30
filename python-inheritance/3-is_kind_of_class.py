6ca73992f2676f7f7b4759450bf8c11e07e02ed1
ramilem8 <13728@holbertonstudents.com>
2026-04-30 20:36:39 +0000

    add

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Checks if object is an instance of a class or inherited from it."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if obj is an instance of a_class or its subclasses."""
+    return isinstance(obj, a_class)
+    
