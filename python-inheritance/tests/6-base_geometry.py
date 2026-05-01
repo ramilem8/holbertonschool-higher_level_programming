4b4c52914517fcff95b1869c61f2654dc6aeec97
ramilem8 <13728@holbertonstudents.com>
2026-05-01 11:25:05 +0000

    add

---
+++ python-inheritance/tests/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    #class for basegeometry
+
+
+    def area(self):
+        """raises an exception"""
+        raise exception("area() is not implemented")
