24d38d9d3b675169c477f2e17b81300aab0cce28
ramilem8 <13728@holbertonstudents.com>
2026-05-04 18:40:06 +0000

    add

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+ """Module that converts an object to JSON string"""
+
+import json:
+
+
+def from_json_string(my_str):
+    "returns JSON represantation of an object"
+    return json.loads(my_str)
