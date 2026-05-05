6f0a713ae4914b69ab0742c1a9b6a03c78361ea5
ramilem8 <13728@holbertonstudents.com>
2026-05-05 13:21:14 +0000

    add

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Returns the dictionary description of an object for JSON serialization."""
 
+
 def class_to_json(obj):
     """Returns dictionary representation of obj."""
     return obj.__dict__
-
-
-
-    
