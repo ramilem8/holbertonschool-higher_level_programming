51da3ccdf05429cea31dc8e3873baf49cb25e23a
ramilem8 <13728@holbertonstudents.com>
2026-05-05 11:17:10 +0000

    add

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"Module that converts JSON string to Python object"
+import json
+
+
+def load_from_json_file(filename):
+    "create a json file"
+    with open(filename,"r", encoding="utf-8")as f:
+        return json.load(f)
