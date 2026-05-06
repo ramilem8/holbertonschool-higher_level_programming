ce9a045808eda1fed731c8ae00e60aba08ea5572
ramilem8 <13728@holbertonstudents.com>
2026-05-06 08:39:47 +0000

    ADD

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Returns Pascal's Triangle of n."""
+
+
+def pascal_triangle(n):
+    """Builds Pascal's triangle."""
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1] * (i + 1)
+
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+
+    return triangle
+    
