ad9635c0e5049d9ea8cafe8752e9f34e1f51b2bc
ramilem8 <13728@holbertonstudents.com>
2026-05-06 08:23:08 +0000

    add

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a Student class."""
+
+class Student:
+    """Student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dictionary representation of the instance.
+        
+        If attrs is a list of strings, only those attributes are returned.
+        Otherwise, return all attributes.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
+        return self.__dict__
