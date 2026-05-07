d8966fdb6ae9a7289a5498ad1fc8920e4a0a2ce6
ramilem8 <13728@holbertonstudents.com>
2026-05-07 07:34:25 +0000

    add

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Basic serialization module."""
+
+import jso
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize a dictionary and save it to a JSON file."""
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file)
+def load_and_deserialize(filename):
+    """Load and deserialize data from a JSON file."""
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
