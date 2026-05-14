1b7712def4ce5b9ea030a916f29f1e248657f8be
ramilem8 <13728@holbertonstudents.com>
2026-05-14 09:16:36 +0000

    add

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of second_table ordered by score desc
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- prints full description of first_table without using DESCRIBE or EXPLAIN
+
+SHOW CREATE TABLE first_table;
