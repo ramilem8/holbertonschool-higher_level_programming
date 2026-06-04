6a9887e50e3b33582723f07a56ac2567af94f2b8
ramilem8 <13728@holbertonstudents.com>
2026-06-04 22:03:57 +0000

    add

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const limit = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 
-if (limit) {
-  for (let i = 0; i < limit; ++i) {
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
