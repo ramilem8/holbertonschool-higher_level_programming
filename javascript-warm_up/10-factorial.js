c8f5f03182ddf51661cc342d6972371855271f0c
ramilem8 <13728@holbertonstudents.com>
2026-06-04 22:13:10 +0000

    add

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
 function factorial (n) {
-    if (n === 0 || isNaN(n)) {
+    if (isNaN(n) || n === 0) {
       return 1;
     }
-  
     return n * factorial(n - 1);
   }
   
-  const n = parseInt(process.argv[2]);
-  console.log(factorial(n));
+  console.log(factorial(parseInt(process.argv[2])));
