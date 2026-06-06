f0a9f04803b2eb7c47a20e24717cf423481fa0e4
ramilem8 <13728@holbertonstudents.com>
2026-06-06 19:50:02 +0000

    add

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -16,11 +16,14 @@ def contact():
     return render_template('contact.html')
 
 @app.route('/items')
+@app.route('/items')
 def items():
     with open('items.json', 'r') as f:
         data = json.load(f)
 
-    return render_template('items.html', items=data['items'])
+    items = data.get('items', [])
+
+    return render_template('items.html', items=items)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
